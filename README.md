d76fbfdc207a1dc255684c313b16ab537bf1ba12
Kesit Budi Kusumo <kusumo1920@icloud.com>
2022-04-11 14:30:59 +0700

    init commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Todo REST API App
+
+This app is To-do REST API app.
+
+Technology: Django, Django Rest Framework (DRF), SQLite.
---
+++ todo_api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TodoDetailApiView, TodoListApiView
+
+urlpatterns = [
+    path('api/', TodoListApiView.as_view()),
+    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
+]
